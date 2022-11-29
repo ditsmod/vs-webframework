eb34efa7db990873e7e281d4f04009fd1475047f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-11-30 01:43:03 +0200

    Adding nodemon

--- nestjs/main-fastify.ts
+++ nestjs/main-fastify.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
