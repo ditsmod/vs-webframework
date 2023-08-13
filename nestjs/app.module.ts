fb5a0587eeffdca54efa16e8d8074a03461a69cc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-13 17:45:37 +0300

    Brought back Scope.REQUEST for NestJS.

--- nestjs/app.module.ts
+++ nestjs/app.module.ts
@@ -2,7 +2,7 @@ import { Module, Scope } from '@nestjs/common';
 import { Controller, Get } from '@nestjs/common';
 
 // @Controller()
-@Controller({ scope: Scope.DEFAULT, durable: false })
+@Controller({ scope: Scope.REQUEST })
 export class AppController {
   @Get('/hello')
   getHello(): string {
